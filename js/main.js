af10d7c562ef74208f6995ca3b0dd441d74aba75
mohamedgaballah99 <64907562+mohamedgaballah99@users.noreply.github.com>
2020-11-29 18:00:59 +0200

    charitywebsite

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    
+   $('.loader').delay(2000).fadeOut("slow");
+    
+});
+
+$("body").niceScroll({
+  cursorcolor:"green",
+  cursorwidth:"16px"
+    })
+
+
+
+
+
+
+
+
+
+
+
+
